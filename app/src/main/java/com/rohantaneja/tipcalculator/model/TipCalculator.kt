3b7d124ce31c1a2a0c4bec877fae7e7f8e1c94d8
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-02 21:25:49 +0530

    added rounding up logic for tipAmount

--- app/src/main/java/com/rohantaneja/tipcalculator/model/TipCalculator.kt
+++ app/src/main/java/com/rohantaneja/tipcalculator/model/TipCalculator.kt
@@ -1,10 +1,16 @@
 package com.rohantaneja.tipcalculator.model
 
+import java.math.RoundingMode
+
 class TipCalculator {
 
     fun calculateTip(billAmount: Double, tipPercentage: Int): TipCalculation {
 
-        val tipAmount = billAmount * (tipPercentage / 100)
+        val tipAmount = billAmount * (tipPercentage.toDouble() / 100)
+                .toBigDecimal()
+                .setScale(2, RoundingMode.HALF_UP)
+                .toDouble()
+
         val grandTotal = billAmount + tipAmount;
 
         return TipCalculation(billAmount, tipPercentage, tipAmount, grandTotal)
